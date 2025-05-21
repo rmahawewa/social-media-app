b81749c6f883ff20063c71882f50cdcfface913d
rmahawewa <rmahawewa@yahoo.com>
2025-05-21 17:06:55 +0530

    install 'cors' npm package and use it as a middleware in the app.js file. Add additional corsOptions => origin: 'url of front end route' , credentials: 'true'

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database.js");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRouter = require("./routes/auth.js");
 const profileRouter = require("./routes/profile.js");
@@ -10,6 +11,9 @@ const userRouter = require("./routes/user.js");
 
 const app = express();
 
+const corsOptions = { origin: "http://localhost:5173", credentials: true };
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,77 +1,78 @@
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
-const { validateSignUpData, validateLoginData } = require("../utils/validation.js");
-const bcrypt = require('bcrypt');
+const {
+	validateSignUpData,
+	validateLoginData,
+} = require("../utils/validation.js");
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 authRouter.post("/signup", async (req, res) => {
-    
-    try {
-        // Validation of data
-        validateSignUpData(req);
+	try {
+		// Validation of data
+		validateSignUpData(req);
 
-        //Encrypting password
-        const { firstName, lastName, emailId, password } = req.body;
-        const passwordHash = await bcrypt.hash(password, 10);
-        console.log(passwordHash);
+		//Encrypting password
+		const { firstName, lastName, emailId, password } = req.body;
+		const passwordHash = await bcrypt.hash(password, 10);
+		console.log(passwordHash);
 
-    
-        // if(!validator.isEmail(req.body.emailId)){
-        //     throw new Error("Email is not valid");
-        // }
-        if(req.body.skills?.length > 10){
-            throw new Error("Skills count can not exeed ten");
-        }
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: passwordHash,
-        });
-        await user.save();
-        res.send("User added successfully");
-    }
-    catch(err){
-        res.status(400).send("ERROR: " + err.message);
-    }
-    
+		// if(!validator.isEmail(req.body.emailId)){
+		//     throw new Error("Email is not valid");
+		// }
+		if (req.body.skills?.length > 10) {
+			throw new Error("Skills count can not exeed ten");
+		}
+		const user = new User({
+			firstName,
+			lastName,
+			emailId,
+			password: passwordHash,
+		});
+		await user.save();
+		res.send("User added successfully");
+	} catch (err) {
+		res.status(400).send("ERROR: " + err.message);
+	}
 });
 
 authRouter.post("/login", async (req, res) => {
-    try{
-        const { emailId, password } = req.body;
-        // Validation of data
-        validateLoginData(req);
+	try {
+		const { emailId, password } = req.body;
+		// Validation of data
+		validateLoginData(req);
 
-        const user = await User.findOne({ emailId: emailId });
-        if(!user){
-            throw new Error("Invalid credencials");
-        }
-        
-        const isValidPassword = await user.validatePassword(password);
-        if(isValidPassword){
-            // Create a JWT Token
-            const token = await user.getJWT();
+		const user = await User.findOne({ emailId: emailId });
+		if (!user) {
+			throw new Error("Invalid credencials");
+		}
 
-            //Add the token to the cookie and send the response back to the user
-            res.cookie("token", token, { expires: new Date(Date.now() + 7 * 24 * 3600000) });
+		const isValidPassword = await user.validatePassword(password);
+		if (isValidPassword) {
+			// Create a JWT Token
+			const token = await user.getJWT();
 
-            res.send("Login Successfull...");
-        }
-        else{
-            throw new Error("Invalid credencials");
-        }
-    }
-    catch(err){
-        res.status(400).send("ERROR: " + err.message );
-    }
+			//Add the token to the cookie and send the response back to the user
+			res.cookie("token", token, {
+				expires: new Date(Date.now() + 7 * 24 * 3600000),
+			});
+
+			res.send(user);
+		} else {
+			throw new Error("Invalid credencials");
+		}
+	} catch (err) {
+		res.status(400).send("ERROR: " + err.message);
+	}
 });
 
 authRouter.post("/logout", async (req, res) => {
-    res.cookie("token", null, { expires: new Date(Date.now()) } ).send("Logout successfull");
-    
-    // OR
-    // res.send();
+	res
+		.cookie("token", null, { expires: new Date(Date.now()) })
+		.send("Logout successfull");
+
+	// OR
+	// res.send();
 });
 
 module.exports = authRouter;
